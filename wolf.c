d3ac4408dc71fd34053b8a7d202ff8b644553ef0
iles982 <iles.boss@mail.ru>
2020-10-22 09:32:38 +0600

    fixed

--- wolf.c
+++ wolf.c
@@ -6,7 +6,7 @@
 /*   By: tclarita <tclarita@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/20 12:14:25 by tclarita          #+#    #+#             */
-/*   Updated: 2020/10/20 10:29:35 by tclarita         ###   ########.fr       */
+/*   Updated: 2020/10/22 09:32:12 by tclarita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,11 @@ int		main(int ac, char **av)
 	sdl->map = read_map(av[1], sdl);
 	init_window(sdl);
 	setup(sdl, player);
-	int i = 1;
-	while (i)
+	while (1)
 	{
 		process(sdl, player);
 		update(sdl, player, ray);
 		render(sdl, player, ray);
-		if (wait(3))
-			i = 0;
 	}
 	destroy_window(sdl);
 	return (0);
